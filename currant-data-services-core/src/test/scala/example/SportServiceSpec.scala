83349bc28ee23aab01c5bc90939eb6ee54444308
Adam Parrish <aparrish@neosavvy.com>
2013-11-19 19:15:32 -0500

    Moving the tests real fast.

--- currant-data-services-core/src/test/scala/example/SportServiceSpec.scala
+++ currant-data-services-core/src/test/scala/example/SportServiceSpec.scala
@@ -25,6 +25,46 @@ class SportServiceSpec extends Specification with DSConfiguration with Specs2Rou
       }
     }
 
+    "return a list of two sports baseball and soccer" in {
+      Get("/sports") ~> sportRoute ~> check {
+        val b = responseAs[String]
+        val sports = read[List[Sport]](b)
+        sports(0) must be equalTo(sports(0))
+      }
+    }
+
+    "allow a new sport parameter to be posted in" in {
+      Post("/sports/new", swrite(new Sport(
+        1L,
+        "New Sport",
+        "Meh just something lame",
+        true,
+        None,
+        None,
+        None,
+        None
+      ))) ~> sportRoute ~> check {
+        val resp = responseAs[String]
+        resp must be equalTo("OK")
+      }
+    }
+
+    //    "allow a get with id to return one sport that matches the id" in {
+    //      Get("/sports/1") ~> sportRoute ~> check {
+    //        val resp = responseAs[String]
+    //        val sport = read[List[Sport]](resp)
+    //        sports(0).id must be equalTo(1)
+    //      }
+    //    }
+
+    "support a delete which should simply return ok" in {
+      Delete("/sports/1") ~> sportRoute ~> check {
+        val resp = responseAs[String]
+        resp must be equalTo("OK")
+      }
+
+    }
+
   }
 
   def db: DB = {
